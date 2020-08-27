808994c6bb90bae913d34b9b1471961c72a1225a
Lenon Corrêa <lenonac_@hotmail.com>
2020-08-27 17:17:16 -0300

    Alteração na API de pesquisa, ajuste de classe e funções

--- cloudflare.py
+++ cloudflare.py
@@ -8,44 +8,69 @@
 # Last updated: 27-08-2020
 #--------------------------------------------------------------------
 
-
-from lxml import html
-# import requests
-# import re
-# import sys
-import cloudscraper
+import requests
+import json
+import sys
 
 # def convertTuple(tup):
 #     str =  ''.join(tup)
 #     return str
 
-locales = ['Porto Alegre, Brazil - (POA)']
+class Cloudflare:
+    def __init__(self):
+        self.baseUrl = 'https://yh6f0r4529hb.statuspage.io/api/v2/summary.json'
+        
+# if len(sys.argv) < 2:
+#     print("erro, informe o site a ser testado\n")
+#     sys.exit(1)
+# site = sys.argv[1]
 
+    def requesting(self):
+        response = requests.get(self.baseUrl)
+        result = response.text.encode('utf8')
+        results = json.loads(result)
+        return results
 
-if len(sys.argv) < 2:
-    print("erro, informe o site a ser testado\n")
-    sys.exit(1)
-site = sys.argv[1]
+    def params(self,results,argument):
+        for component in results:
+            component = results['components']
+            for ids in component:
+                city = component[id]
+                return city
+            #     # if (component == '7k05x28lndzb'):
+                #     print(id)
+            # version = results['0']
+            # return city
+    
+def main():
+    cloud = Cloudflare()
+    results = cloud.requesting()
+#   result = cloud.params(results,argument)
+    cities = {"data": [{"city": "POA", "id": "96"}, {"city": "FOR", "id": "48"}]}
+    for city in cities:
+        # if cities['data']['city']
+        print(cities['data']['city'])
+    # cities = json.dumps(cities)
+    # print(cities)
+#   print(result)
 
-baseUrl = 'https://www.cloudflarestatus.com/'
-scraper = cloudscraper.create_scraper()
-page = scraper.get(baseUrl)
-# print(page)
-
+if __name__ == '__main__':
+  argument = sys.argv[1]
+  result = main()
 
 # user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 # headers = {'User-Agent': user_agent}
 # page = requests.get(url,headers=headers)
 # page = requests.get(url)
 # print(response)
-if page.status_code != 200:
-    # print('Erro: '+page.status_code)
-    print("0")
-    sys.exit(1)
+# if page.status_code != 200:
+#     # print('Erro: '+page.status_code)
+#     print("0")
+#     sys.exit(1)
   
-doc = html.fromstring(page.content)
+# doc = html.fromstring(page.content)
 
-for locale in locales:
+# for locale in locales:
 #   city = tree.xpath('//span[contains(text(), "{}")]/text()'.format(locale))
 #   status = tree.xpath('//span[@class="component-status "]/text()')
 #   print(status)
